834623875833a0b13851a4c81edc1433d92c8fd2
Himanshu Dixit <hudixt@gmail.com>
2022-11-03 10:02:33 +0000

    Initial commit- test streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,20 @@
 import pathlib
 import subprocess
+import os
 
 import ffmpeg
 import streamlit as st
 
 # global variables
-uploaded_mp3_file = None
-uploaded_mp3_file_length = 0
+uploaded_mp4_file = None
+uploaded_mp4_file_length = 0
 filename = None
 downloadfile = None
 
+def save_uploaded_file(uploadedfile):
+  with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
+     f.write(uploadedfile.getbuffer())
+  return st.success("Saved file :{} in tempDir".format(uploadedfile.name))
 
 @st.experimental_memo
 def convert_mp3_to_wav_ffmpeg_bytes2bytes(input_data: bytes) -> bytes:
@@ -32,32 +37,37 @@ def convert_mp3_to_wav_ffmpeg_bytes2bytes(input_data: bytes) -> bytes:
 
 
 @st.experimental_memo
-def on_file_change(uploaded_mp3_file):
-    return convert_mp3_to_wav_ffmpeg_bytes2bytes(uploaded_mp3_file.getvalue())
+def on_file_change(uploaded_mp4_file):
+    print(uploaded_mp4_file)
+    # with open("tmp/uploaded_mp4_file.mp4", "w") as f:   # Opens file and casts as f 
+    #     f.write("Hello World form " + f.name) 
+
+    save_uploaded_file(uploaded_mp4_file)
+    output = subprocess.getoutput("ls -l")
+    # return convert_mp3_to_wav_ffmpeg_bytes2bytes(uploaded_mp4_file.getvalue())
 
 
 def on_change_callback():
     """
     It prints a message to the console. Just for testing of callbacks.
     """
-    print(f'on_change_callback: {uploaded_mp3_file}')
+    print(f'on_change_callback: {uploaded_mp4_file}')
 
 
 # The below code is a simple streamlit web app that allows you to upload an mp3 file
 # and then download the converted wav file.
 if __name__ == '__main__':
-    st.title('MP3 to WAV Converter test app')
-    st.markdown("""This is a quick example app for using **ffmpeg** on Streamlit Cloud.
-    It uses the `ffmpeg` binary and the python wrapper `ffmpeg-python` library.""")
+    st.title('HQ Video to gif')
+    st.markdown("""Convert video to gif with custom palette. Generally high quality""")
 
-    uploaded_mp3_file = st.file_uploader('Upload Your MP3 File', type=['mp3'], on_change=on_change_callback)
+    uploaded_mp4_file = st.file_uploader('Upload Your MP4 File', type=['mp4'], on_change=on_change_callback, accept_multiple_files=0)
 
-    if uploaded_mp3_file:
-        uploaded_mp3_file_length = len(uploaded_mp3_file.getvalue())
-        filename = pathlib.Path(uploaded_mp3_file.name).stem
-        if uploaded_mp3_file_length > 0:
-            st.text(f'Size of uploaded "{uploaded_mp3_file.name}" file: {uploaded_mp3_file_length} bytes')
-            downloadfile = on_file_change(uploaded_mp3_file)
+    if uploaded_mp4_file:
+        uploaded_mp4_file_length = len(uploaded_mp4_file.getvalue())
+        filename = pathlib.Path(uploaded_mp4_file.name).stem
+        if uploaded_mp4_file_length > 0:
+            st.text(f'"{uploaded_mp4_file.name}": {round(uploaded_mp4_file_length/(1000 * 1000),2)} Mb')
+            downloadfile = on_file_change(uploaded_mp4_file)
 
     st.markdown("""---""")
     if downloadfile:
